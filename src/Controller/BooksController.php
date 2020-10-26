3ace0475a8fbd9319b007291e36b4c18bc648322
Loghorrean <loghorrean74@gmail.com>
2020-10-26 18:06:17 +0300

    Reworked the controller due to CI standarts

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -10,21 +10,23 @@ class BooksController
     /**
      * @Route("/")
      */
-    public function index() {
+    public function index() : Response {
         return new Response("Test controller");
     }
 
     /**
      * @Route("/add")
      */
-    public function add_book() {
+    public function add_book() : Response {
         return new Response("Method to add a book");
     }
 
     /**
      * @Route("/edit/{book_id}")
+     * @param int $book_id
+     * @return Response
      */
-    public function edit_book($book_id) {
+    public function edit_book(int $book_id) : Response {
         return new Response(sprintf("Method to edit a book with the id = %s",
             $book_id));
     }
