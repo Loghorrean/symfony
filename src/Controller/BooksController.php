402290c1e15c4e5e9eed1e986ada12334304963b
Loghorrean <loghorrean74@gmail.com>
2020-10-27 21:33:43 +0300

    Finished work

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Author;
 use App\Entity\Book;
 use App\Form\AuthorType;
+use App\Form\BookType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -18,28 +19,29 @@ class BooksController extends AbstractController
      * @Route("/", name="main_page")
      */
     public function index() : Response {
-        $books = $this->getDoctrine()->getRepository(Book::class)->getBooksWithAuthors();
+        $books = $this->getDoctrine()->getRepository(Book::class)->findAll();
         return $this->render('Books/book_main.html.twig', ['books' => $books]);
     }
 
     /**
      * @Route("/add", name="add_a_book")
      */
-    public function add_book() : Response {
-        return $this->render('Books/book_add.html.twig');
-    }
+    public function add_book(Request $request) : Response {
+        $book = new Book();
+        $em = $this->getDoctrine()->getManager();
 
-    /**
-     * @Route("/edit/{book_id<\d+>}", name="edit_a_book")
-     * @param int $book_id
-     * @return Response
-     */
-    public function edit_book(int $book_id) : Response {
-        $book = $this->getDoctrine()->getRepository(Author::class)->find($book_id);
-        if (!$book) {
-            throw $this->createNotFoundException("No book with an id " . $book_id);
+        $books = $this->getDoctrine()->getRepository(Book::class)->findAll();
+
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($book);
+            $em->flush();
+            $this->addFlash('add_book_success', 'Book is successfully added');
+            return $this->redirectToRoute('add_a_book');
         }
-        return $this->render('Books/book_edit.html.twig', ['book_id' => $book->getId(), 'author_name' => $book->getName()]);
+        return $this->render('Books/book_add.html.twig', ['books' => $books, 'form' => $form->createView()]);
     }
 
     /**
@@ -60,8 +62,33 @@ class BooksController extends AbstractController
             $author = $form->getData();
             $em->persist($author);
             $em->flush();
-            return $this->redirectToRoute('main_page');
+            $this->addFlash('add_author_success', 'Author is successfully added');
+            return $this->redirectToRoute('add_an_author');
         }
         return $this->render('Author/author_add.html.twig', ['authors' => $authors, 'form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/edit/{book_id<\d+>}", name="edit_a_book")
+     * @param int $book_id
+     * @param Request $request
+     * @return Response
+     */
+    public function edit_book(int $book_id, Request $request) : Response {
+        $em = $this->getDoctrine()->getManager();
+
+        $book = $this->getDoctrine()->getRepository(Book::class)->find($book_id);
+        if (!$book) {
+            throw $this->createNotFoundException("No book with an id " . $book_id);
+        }
+
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('edit_book_success', 'Book is successfully edited');
+            $em->flush();
+            return $this->redirectToRoute('main_page');
+        }
+        return $this->render('Books/book_edit.html.twig', ['form' => $form->createView()]);
     }
 }
--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -2,6 +2,8 @@
 
 namespace App\Form;
 
+use App\Entity\Author;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -11,19 +13,24 @@ use Symfony\Component\Form\FormBuilderInterface;
 class BookType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) : void {
         $builder->
-            add('title', TextType::class,
+            add('name', TextType::class,
             array('label' => 'Title of the book',
                 'attr' => array(
                     'class' => 'form-control')
             ))
-            ->add('date_of_publish', DateType::class,
-            array('label' => 'Date of publishing the book',
+            ->add('date_of_publish', DateType::class, array(
+                'label' => 'Date of publishing the book',
                 'attr' => array(
                     'class' => 'form-control')
             ))
-            ->add('save', SubmitType::class,
-            array('attr' => array(
-                'class' => 'btn btn-primary'
+            ->add('author_id', EntityType::class, array(
+                'label' => 'Name of author',
+                'class' => Author::class,
+                'choice_label' => 'name'
+                ))
+            ->add('save', SubmitType::class, array(
+                'attr' => array(
+                    'class' => 'btn btn-primary'
             )
             ));
     }
