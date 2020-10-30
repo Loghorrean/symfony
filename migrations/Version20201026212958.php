4538237c7c0c042f8fa29edf0b9ef097a4dacfb5
Loghorrean <loghorrean74@gmail.com>
2020-10-30 18:24:55 +0300

    Reworked the migrations manually

--- migrations/Version20201026212958.php
+++ migrations/Version20201026212958.php
@@ -7,30 +7,16 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-final class Version20201026212958 extends AbstractMigration
-{
-    public function getDescription() : string
-    {
-        return '';
+final class CreatingBookTable extends AbstractMigration {
+    public function getDescription() : string {
+        return 'Creating book table';
     }
 
-    public function up(Schema $schema) : void
-    {
-        // this up() migration is auto-generated, please modify it to your needs
+    public function up(Schema $schema) : void {
+        $this->addSql("CREATE TABLE book (id INT PRIMARY KEY AUTO_INCREMENT, author_id INT NOT NULL, name varchar(35) NOT NULL, date_of_publish DATE NOT NULL, FOREIGN KEY (author_id) REFERENCES author(id))");
+    }
+
+    public function down(Schema $schema) : void {
         $this->addSql('DROP TABLE book');
-        $this->addSql('DROP INDEX author_name ON author');
-        $this->addSql('ALTER TABLE author CHANGE author_name name VARCHAR(35) NOT NULL');
-    }
-
-    public function down(Schema $schema) : void
-    {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE book (id INT AUTO_INCREMENT NOT NULL, author_id INT NOT NULL, book_name VARCHAR(35) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, year_of_publish DATE NOT NULL, INDEX author_id (author_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE book ADD CONSTRAINT book_ibfk_1 FOREIGN KEY (author_id) REFERENCES author (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('ALTER TABLE author CHANGE name author_name VARCHAR(35) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`');
-        $this->addSql('CREATE UNIQUE INDEX author_name ON author (author_name)');
     }
 }
--- migrations/Version20201026230422.php
+++ migrations/Version20201026230422.php
@@ -7,26 +7,18 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-final class Version20201026230422 extends AbstractMigration
-{
-    public function getDescription() : string
-    {
-        return '';
+final class CreatingAuthorTable extends AbstractMigration {
+    public function getDescription() : string {
+        return 'Creating author table';
     }
 
-    public function up(Schema $schema) : void
-    {
+    public function up(Schema $schema) : void {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE book (id INT AUTO_INCREMENT NOT NULL, author_id_id INT NOT NULL, name VARCHAR(35) NOT NULL, date_of_publish DATE NOT NULL, INDEX IDX_CBE5A33169CCBE9A (author_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE book ADD CONSTRAINT FK_CBE5A33169CCBE9A FOREIGN KEY (author_id_id) REFERENCES author (id)');
+        $this->addSql("CREATE TABLE author (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(35) NOT NULL UNIQUE)");
     }
 
-    public function down(Schema $schema) : void
-    {
+    public function down(Schema $schema) : void {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE book');
+        $this->addSql('DROP TABLE author');
     }
 }
